7aa6c239efb65815e29b7f0cf7d7e7e811183b1b
Román Martínez <roman.mx3@gmail.com>
2020-05-12 22:01:00 -0500

    Make webpack.config.js return a simple object

--- webpack.config.js
+++ webpack.config.js
@@ -3,81 +3,79 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const miniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-module.exports = env => {
-	const isDevelopment = process.env.NODE_ENV !== 'production';
-	return {
-		...defaultConfig,
-		entry: {
-			index: path.resolve( process.cwd(), 'src', 'index.js' ),
-			frontend: path.resolve( process.cwd(), 'src', 'frontend.js' ),
-		},
-		module: {
-			...defaultConfig.module,
-			rules: [
-				...defaultConfig.module.rules,
-				{
-					test: /\.s?css$/,
-					include: __dirname+'/src',
-					use: [
-						{
-							loader: miniCssExtractPlugin.loader,
-						},
-						{
-							loader: 'css-loader',
-							options: {
-								url: false,
-								import: true,
-							},
-						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sassOptions: {
-									outputStyle: 'expanded',
-								},
-							},
-						},
-					],
-				},
-			],
-		},
-		optimization: {
-			...defaultConfig.optimization,
-			splitChunks: {
-				cacheGroups: {
-					style: {
-						name: 'style',
-						test: /style\.scss$/,
-						chunks: 'async',
-						enforce: true,
-					},
-					editor: {
-						name: 'editor',
-						test: /editor\.scss$/,
-						chunks: 'async',
-						enforce: true,
-					},
-				},
-			},
-		},
-		plugins: [
-			...defaultConfig.plugins,
-			new CleanWebpackPlugin(),
-			new miniCssExtractPlugin({
-				filename: '[name].css',
-			}),
-			isDevelopment ? null : new OptimizeCSSAssetsPlugin(),
+const isDevelopment = process.env.NODE_ENV !== 'production';
+module.exports = {
+	...defaultConfig,
+	entry: {
+		index: path.resolve( process.cwd(), 'src', 'index.js' ),
+		frontend: path.resolve( process.cwd(), 'src', 'frontend.js' ),
+	},
+	module: {
+		...defaultConfig.module,
+		rules: [
+			...defaultConfig.module.rules,
 			{
-				apply( compiler ) {
-					compiler.hooks.shouldEmit.tap( 'Remove js files created from css chunks', compilation => {
-						delete compilation.assets['style.js'];
-						delete compilation.assets['style.js.map'];
-						delete compilation.assets['editor.js'];
-						delete compilation.assets['editor.js.map'];
-						return true;
-					});
-				}
+				test: /\.s?css$/,
+				include: __dirname+'/src',
+				use: [
+					{
+						loader: miniCssExtractPlugin.loader,
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							url: false,
+							import: true,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sassOptions: {
+								outputStyle: 'expanded',
+							},
+						},
+					},
+				],
 			},
-		].filter( Boolean ),
-	}
+		],
+	},
+	optimization: {
+		...defaultConfig.optimization,
+		splitChunks: {
+			cacheGroups: {
+				style: {
+					name: 'style',
+					test: /style\.scss$/,
+					chunks: 'async',
+					enforce: true,
+				},
+				editor: {
+					name: 'editor',
+					test: /editor\.scss$/,
+					chunks: 'async',
+					enforce: true,
+				},
+			},
+		},
+	},
+	plugins: [
+		...defaultConfig.plugins,
+		new CleanWebpackPlugin(),
+		new miniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+		isDevelopment ? null : new OptimizeCSSAssetsPlugin(),
+		{
+			apply( compiler ) {
+				compiler.hooks.shouldEmit.tap( 'Remove js files created from css chunks', compilation => {
+					delete compilation.assets['style.js'];
+					delete compilation.assets['style.js.map'];
+					delete compilation.assets['editor.js'];
+					delete compilation.assets['editor.js.map'];
+					return true;
+				});
+			}
+		},
+	].filter( Boolean ),
 }
